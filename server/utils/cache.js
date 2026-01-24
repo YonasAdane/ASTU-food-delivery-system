802d99345535b0ba6596847e0369c04d5eb55d81
azarielTesfaye <azarieltesfaye2021@gmail.com>
2026-01-24 13:11:42 +0300

    fix: improve cart and orders controllers

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,9 +1,9 @@
 const { client: redisClient } = require("../config/redis");
-
+const logger = require("../utils/logger");
 const checkCache = async (cacheKey) => {
   try {
     const cached = await redisClient.get(cacheKey);
-    return cached ? JSON.parse(cacheKey) : null;
+    return cached ? JSON.parse(cached) : null;
   } catch (err) {
     logger.error(`Error checking cache for key ${cacheKey}: ${err.message}`);
     return null;
