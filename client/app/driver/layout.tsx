464804c7d986c3c0f82aa2c976c33a2a38d06662
yohanzack <yohanzack9@gmail.com>
2026-01-26 00:33:09 +0300

    Refactor driver dashboard and layout components: streamline code, remove unused imports, and enhance readability.

--- client/app/driver/layout.tsx
+++ client/app/driver/layout.tsx
@@ -1,10 +1,10 @@
 "use client";
-
 import DriverLocationTracker from "@/components/drivers/DriverLocationTracker";
 import { useUserStore } from "@/hooks/use-profile";
 import DriverNavBar from "@/components/drivers/DriverNavbar";
 import { ModeToggle } from "@/components/common/modeToggle";
 import ProfileDropdown from "@/components/users/profileDropdown";
+
 
 const generateAvatar = (name?: string, email?: string) => {
   const displayName = name || email || "User";
@@ -24,18 +24,14 @@ export default function DriverLayout({
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-slate-950">
       
-      {/* 1. Tracker (Invisible Logic) */}
       {driverId && <DriverLocationTracker driverId={driverId} />}
-
-      {/* 2. Navigation (Sidebar on Desktop, Bottom Bar on Mobile) */}
       <DriverNavBar />
-      <main className="md:pl-64 pb-24 transition-all duration-300 min-h-screen ">
+
+      <main className="md:pl-64 pb-24 transition-all duration-300 min-h-screen">
         
-    
-    
-        {/* --- PAGE CONTENT --- */}
-        <div className="max-w-7xl mx-auto w-full p-4 md:p-6 ">
-           <div className="flex flex-row items-center gap-3">
+        <div className="max-w-7xl mx-auto w-full p-4 md:p-6 relative">
+           
+           <div className="absolute top-4 right-4 md:top-6 md:right-6 flex flex-row items-center gap-3 z-10">
             <ModeToggle />
             <ProfileDropdown
               avatar={generateAvatar(user?.firstName, user?.email)}
@@ -45,7 +41,9 @@ export default function DriverLayout({
               createdAt={user?.createdAt}
             />
           </div>
+
           {children}
+          
         </div>
         
       </main>
